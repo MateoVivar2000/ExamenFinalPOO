be151e07cb8b3cd7bfd91fffe785a1e0dfe831cc
Mateo Vivar <mateovivar68@gmail.com>
2025-12-02 15:50:44 -0500

    inicio del proyecto

--- ExamenPractico/src/prueba/tablero.java
+++ ExamenPractico/src/prueba/tablero.java
@@ -1,35 +1,5 @@
 package prueba;
 
 public class tablero {
-	//aqui va lo del tablero
-	//commit
-	///ajhgsd
-	/////mateo jaja
-	/////010101
-	////5465
-	//544
-<<<<<<< HEAD
-	//alexander
-=======
->>>>>>> branch 'master' of https://github.com/MateoVivar2000/ExamenFinalPOO.git
+	
 }
-//hhhhh
-/// vale?
-//oka
-//push
-<<<<<<< HEAD
-//tablero
-=======
-<<<<<<< HEAD
-// sakjkhjsdajsdahdaas
-//sfdakjhsdakjasd
-=======
-// estoy loco 
-//no te olvides de token jaja 
-<<<<<<< HEAD
-////tomemos un traguito
-///////tomemos
->>>>>>> branch 'master' of https://github.com/MateoVivar2000/ExamenFinalPOO.git
-
-	//hola java
-	volvi
