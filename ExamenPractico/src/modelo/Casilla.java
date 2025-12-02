34a3897a91b7f20a3434cc597b7deb442e33a9a9
Mateo Vivar <mateovivar68@gmail.com>
2025-12-02 18:29:00 -0500

    commit previo a finalizacion de clase

--- ExamenPractico/src/modelo/Casilla.java
+++ ExamenPractico/src/modelo/Casilla.java
@@ -5,7 +5,7 @@ package modelo;
  
 public class Casilla {
 
-    // Atributos 
+    // ==============Atributos=========== 
     private boolean tieneMina;
     private boolean estaDescubierta;
     private boolean estaMarcada; 
@@ -13,63 +13,68 @@ public class Casilla {
     private final int fila;
     private final int columna;
 
-//     * Constructor
-     /* @param fila Fila de la casilla (0-9)
-     * @param columna Columna de la casilla (0-9)*/
-     */
+    // ================Constructor==============
+     
     public Casilla(int fila, int columna) {
         this.fila = fila;
         this.columna = columna;
+        //A PESAR DE NO SER UN PARAMETRO A RECIBIR, COMO BUENA PRACTICA SE DEBE INICAR EN 0 O FALSO
         this.tieneMina = false;
         this.estaDescubierta = false;
         this.estaMarcada = false;
         this.minasAdyacentes = 0;
     }
 
-    // --- Métodos Modificadores (Mutators/Setters) ---
-
+    //=============Métodos Modificadores==============
+    
+    //METODO QUE COLOCA LA MINA
     public void colocarMina() {
         this.tieneMina = true;
     }
-
+    
+    // METODO DE CORRECTO NUMERO DE MINAS
     public void setMinasAdyacentes(int minasAdyacentes) {
         this.minasAdyacentes = minasAdyacentes;
     }
     
-    // Método clave que cambia el estado al ser seleccionada
-    public void descubrir() {
+    // METODO PARA CAMBIAR EL ESTADO DE LA CASILLA
+    public void desmarcar() {
         this.estaDescubierta = true;
     }
     
-    // Método para marcar/desmarcar con bandera (requisito del juego)
+    // METODO PARA MARCAR O DESMARCAR CASILLA
     public void marcar() {
-        if (!estaDescubierta) { // Solo se puede marcar si no ha sido descubierta
+        if (!estaDescubierta) { // Solo se puede marcar si no ha sido desmarcada
              this.estaMarcada = !this.estaMarcada; 
         }
     }
 
-    // --- Métodos Accesores (Accessors/Getters) ---
+    // ================METODOS DE ACCESO==================
 
+    //METODO SI TIENE MINA DEVUELVE QUE TIENE MINA ¿TIENE MINA? SI/NO
     public boolean tieneMina() {
         return tieneMina;
     }
-
+    
+    //METODO PREGUNTA ESTA DESCUBIERTO SI/NO
     public boolean estaDescubierta() {
         return estaDescubierta;
     }
 
+  //METODO PREGUNTA ESTA MARCADA SI/NO
     public boolean estaMarcada() {
         return estaMarcada;
     }
-
+    
+    //METODO DEVUELVE EL NUMERO DE MINAS ADYACENTES
     public int getMinasAdyacentes() {
         return minasAdyacentes;
     }
     
+    //OBTIENE LA UBICACIÓN
     public int getFila() {
         return fila;
     }
-
     public int getColumna() {
         return columna;
     }
@@ -80,15 +85,19 @@ public class Casilla {
      */
     @Override
     public String toString() {
-        if (estaMarcada) {
-            return "F"; // Bandera (Flag)
+        //SI ESTA MARCADA COLOCA
+    	if (estaMarcada) {
+            return "\nU+2713"; // retorna un visto
         }
+    	//si no esta decubierta tapoa la casilla
         if (!estaDescubierta) {
             return "☐"; // Casilla cubierta
         }
+        //si tiene mina devuelve una X
         if (tieneMina) {
             return "X"; // Ubicación de una mina
         }
+        //
         if (minasAdyacentes > 0) {
             return String.valueOf(minasAdyacentes); // Muestra el número
         }
